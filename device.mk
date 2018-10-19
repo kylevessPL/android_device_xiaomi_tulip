00508dbefacd31873e6aac935aa4f812208c90a9
Luca Stefani <luca.stefani.ge1@gmail.com>
2018-10-19 16:59:43 +0200

    tulip: Build NoCutoutOverlay
    Change-Id: I45edeb3ae20f374cd4510bad83b3a51059a7e743

--- device.mk
+++ device.mk
@@ -28,8 +28,10 @@ $(call inherit-product, vendor/xiaomi/tulip/tulip-vendor.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-aosip
+    $(LOCAL_PATH)/overlay
+
+PRODUCT_PACKAGES += \
+    NoCutoutOverlay
 
 PRODUCT_ENFORCE_RRO_TARGETS := \
     framework-res
